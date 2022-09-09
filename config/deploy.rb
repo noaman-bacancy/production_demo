1f7f90e47880151c95b1eba3f74b911e2def27d4
Your Name <you@example.com>
2022-09-09 13:31:12 +0000

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :application, 'test'
 set :repo_url, 'https://github.com/noaman-bacancy/production_demo.git'
 
 # Deploy to the user's home directory
-set :deploy_to, "/home/deploy/#{fetch :application}"
+set :deploy_to, "/home/vagrant/test"
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
